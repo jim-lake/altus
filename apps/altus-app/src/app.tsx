4a3ee15507402687d15db3441f088aaef1a29085
jlake <jim@blueskylabs.com>
2026-06-17 13:30:13 -0700

    altus framework

---
+++ apps/altus-app/src/app.tsx
@@ -0,0 +1,28 @@
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'black',
+    fontSize: 30,
+  },
+});
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle='light-content' />
+      <View style={styles.container}>
+        <Text style={styles.text}>Test</Text>
+      </View>
+    </SafeAreaProvider>
+  );
+}
