3264c61ba9acb4ca157d5ac356cee1df63d1fb4a
jlake <jim@blueskylabs.com>
2026-06-22 15:09:39 -0700

    trace log

--- apps/altus-app/src/tools/log.ts
+++ apps/altus-app/src/tools/log.ts
@@ -4,18 +4,23 @@ export default {
   log,
   errorLog,
   errorQuietLog,
+  traceLog,
   debugLog,
 };
 
 export type Logger = (...args: unknown[]) => void;
 let g_logger: Logger | null = null;
 let g_enabled = true;
+let g_traceEnabled = true;
 
 export function setDebugLogger(logger: Logger) {
   g_logger = logger;
 }
 export function setLoggingEnabled(enabled: boolean) {
   g_enabled = enabled;
+}
+export function setTraceLoggingEnabled(enabled: boolean) {
+  g_traceEnabled = enabled;
 }
 export function log(...args: unknown[]) {
   if (!g_enabled) {
@@ -36,6 +41,16 @@ export function errorLog(...args: unknown[]) {
 }
 export function errorQuietLog(...args: unknown[]) {
   if (!g_enabled) {
+    return;
+  }
+  // eslint-disable-next-line no-console
+  console.log(...args);
+  if (g_logger) {
+    g_logger(...args);
+  }
+}
+export function traceLog(...args: unknown[]) {
+  if (!g_enabled || !g_traceEnabled) {
     return;
   }
   // eslint-disable-next-line no-console
